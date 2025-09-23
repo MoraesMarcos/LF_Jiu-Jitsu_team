4d6584331022c77b1e1191199782756b15cc9e8b
Wellingtin-Notebook <wvfr@discente.ifpe.edu.br>
2025-09-23 14:22:07 -0300

    Adiciona seção 'Sobre' e ajusta a navegação

--- lf_jiu-jitsu/src/components/AboutSection.vue
+++ lf_jiu-jitsu/src/components/AboutSection.vue
@@ -1,17 +1,20 @@
 <template>
     <section class="about">
         <div class="container">
-            <div class="about-image"></div>
+            <div class="about-image">
+                <img src="@/assets/images/publico/foto_equipe.jpeg" alt="Foto da equipe">
+            </div>
             <div class="about-content">
                 <h2>Nossa Casa, Sua Família</h2>
                 <p>Acreditamos que o Jiu-Jitsu vai além do tatame. Construímos um ambiente de respeito mútuo, onde cada aluno é parte fundamental da nossa equipe. Venha fazer parte.</p>
-                <a href="#" class="btn btn-secondary">Conheça Nossa História</a>
+                <RouterLink to="/sobre" class="btn btn-secondary">Conheça Nossa História</RouterLink>
             </div>
         </div>
     </section>
 </template>
 
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <style scoped>
@@ -25,6 +28,12 @@
     height: 400px;
     background-color: var(--border-color);
     border-radius: 12px;
+    overflow: hidden; 
+}
+.about .about-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 .about .about-content {
     flex: 1;
